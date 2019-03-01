da7df9115706ae547be22684dd968248e2cd6e0b
Willian <Willian@DESKTOP-B1T9NO5>
2019-03-01 13:16:59 -0300

    ok

---
+++ WebApiVueJs/Curso VueJs/js/app.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        pokemonList: [
+            {name: 'Charizard', number: '006'},
+            {name: 'Squirtle', number: '007'},
+            {name: 'Pikachu', number: '025'},
+            {name: 'Celebi', number: '251'},
+            {name: 'Lucario', number: '448'},
+            {name: 'Delphox', number: '655'},
+        ]
+    }
+})
